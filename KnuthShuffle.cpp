c38acdf6447872155a094b098f3e383ab3fcc629
Rohan Goyal <rohangoyal1407@gmail.com>
2018-01-15 01:30:06 +0530

    adding a new file KnuthShuffle.cpp and have written some code to create an array and display it on the screen

---
+++ KnuthShuffle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(void)
+{
+  int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,
+    14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+  cout<<"\n UnShuffled Array:\n";
+  for(int j=0;j<30;j++)
+  {
+    cout<<" "<<arr[j]<<" ";
+  }
+  
+  return 0;
+}
