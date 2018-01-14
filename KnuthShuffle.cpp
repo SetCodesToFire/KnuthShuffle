e60f3f13fa9d8a2d7a0e73a85f1582299eba3a8f
Rohan Goyal <rohangoyal1407@gmail.com>
2018-01-15 01:36:38 +0530

    added random seed for better implementation

--- KnuthShuffle.cpp
+++ KnuthShuffle.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 int main(void)
 {
+  srand (time(NULL));
   int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,
     14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
   cout<<"\n UnShuffled Array:\n";
